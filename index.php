6b04569e41ec27ca35f25116100f11fa6a1ac165
Marcelo Alencar <marceloalencar@users.noreply.github.com>
2022-08-27 23:22:19 -0300

    Footer: Implementação do menu Mapa do Site

--- index.php
+++ index.php
@@ -215,61 +215,11 @@ $logo_footer = $this->params->get('cor_footer') ? 'media/templates/site/govbr-ds
             <footer class="<?php echo $cor_footer ?>" id="footer">
 				<div class="<?php echo $largura ?>">
 					<div class="logo"><img src="<?php echo $this->params->get('imagem_logo_footer', $logo_footer); ?>" alt="Imagem"/></div>
-					<div class="br-list horizontal" data-toggle="data-toggle" data-sub="data-sub">
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 1</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Ad deserunt nostrud</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Nulla occaecat eiusmod</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Nulla occaecat eiusmod</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 2</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Ex qui laborum consectetur aute commodo</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 3</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Adipisicing culpa et ad consequat</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Nulla occaecat eiusmod</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 4</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Qui esse</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Duis incididunt consectetur</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 5</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Ad deserunt nostrud</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Adipisicing culpa et ad consequat</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-						<div class="col-2"><a class="br-item header" href="javascript:void(0)">
-							<div class="content text-down-01 text-bold text-uppercase">Categoria 6</div>
-							<div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i></div></a>
-							<div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item" href="javascript:void(0)">
-								<div class="content">Adipisicing culpa et ad consequat</div></a><a class="br-item" href="javascript:void(0)">
-								<div class="content">Qui esse</div></a><span class="br-divider d-md-none"></span>
-							</div>
-						</div>
-					</div>
+                    <?php if ($this->countModules('menuacesso')) : ?>
+                        <div class="br-list horizontal" data-toggle="data-toggle" data-sub="data-sub">
+                            <jdoc:include type="modules" name="menumapa" style="none" />
+                        </div>
+                    <?php endif; ?>
 					<div class="d-none d-sm-block">
 						<div class="row align-items-end justify-content-between py-5">
 							<div class="col social-network">
